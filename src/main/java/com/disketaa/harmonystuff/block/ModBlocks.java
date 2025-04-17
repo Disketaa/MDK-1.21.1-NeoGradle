3dea90d19671526f6712d1b4158d873afae9e73c
Disketaa <76956762+Disketaa@users.noreply.github.com>
2025-04-17 20:59:24 +0300

    0.3

--- src/main/java/com/disketaa/harmonystuff/block/ModBlocks.java
+++ src/main/java/com/disketaa/harmonystuff/block/ModBlocks.java
@@ -4,7 +4,6 @@ import com.disketaa.harmonystuff.HarmonyStuff;
 import com.disketaa.harmonystuff.item.ModItems;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -23,90 +22,81 @@ import java.util.function.Supplier;
 public class ModBlocks {
 	public static final DeferredRegister.Blocks	BLOCKS =
 		DeferredRegister.createBlocks(HarmonyStuff.MOD_ID);
-
 	// PROPERTIES
-	private static final BlockBehaviour.Properties TIN =
-		BlockBehaviour.Properties.of()
+	private static final class Properties {
+
+		static final BlockBehaviour.Properties GENERIC_TIN = BlockBehaviour.Properties.of()
 			.mapColor(MapColor.TERRACOTTA_WHITE)
+			.sound(SoundType.COPPER)
 			.requiresCorrectToolForDrops()
-			.strength(2.0f)
-			.explosionResistance(6.0f)
-			.sound(SoundType.COPPER);
+			.strength(2.0f, 6.0f);
+
+		static final BlockBehaviour.Properties ORE_STONE = BlockBehaviour.Properties.of()
+			.mapColor(MapColor.STONE)
+			.instrument(NoteBlockInstrument.BASEDRUM)
+			.requiresCorrectToolForDrops()
+			.strength(3.0f, 3.0f);
+
+		static final BlockBehaviour.Properties ORE_RAW = BlockBehaviour.Properties.of()
+			.mapColor(MapColor.RAW_IRON)
+			.requiresCorrectToolForDrops()
+			.strength(5.5f, 6.0f);
+
+		static final BlockBehaviour.Properties ORE_DEEPSLATE = BlockBehaviour.Properties.of()
+			.mapColor(MapColor.DEEPSLATE)
+			.sound(SoundType.DEEPSLATE)
+			.requiresCorrectToolForDrops()
+			.strength(4.5f, 3.0f);
+	}
 
 	// BLOCKS
-	public static final DeferredBlock<Block> TIN_BLOCK = registerBlock("tin_block",
-		() -> new Block(TIN));
+	public static final DeferredBlock<Block> TIN_BLOCK = registerTinBlock("tin_block");
+	public static final DeferredBlock<Block> CHISELED_TIN = registerTinBlock("chiseled_tin");
+	public static final DeferredBlock<Block> CUT_TIN = registerTinBlock("cut_tin");
+	public static final DeferredBlock<Block> RAW_TIN_BLOCK = registerBlock("raw_tin_block", () -> new Block(Properties.ORE_RAW));
+	public static final DeferredBlock<Block> TIN_GRATE = registerBlock("tin_grate", () -> createGrateBlock(TIN_BLOCK.get()));
+	public static final DeferredBlock<StairBlock> CUT_TIN_STAIRS = registerBlock("cut_tin_stairs", () -> new StairBlock(CUT_TIN.get().defaultBlockState(), Properties.GENERIC_TIN));
+	public static final DeferredBlock<SlabBlock> CUT_TIN_SLAB = registerBlock("cut_tin_slab", () -> new SlabBlock(Properties.GENERIC_TIN));
+	public static final DeferredBlock<DoorBlock> TIN_DOOR = registerBlock("tin_door", () -> new DoorBlock(BlockSetType.COPPER, createDoorProperties(TIN_BLOCK.get())));
+	public static final DeferredBlock<TrapDoorBlock> TIN_TRAPDOOR = registerBlock("tin_trapdoor", () -> new TrapDoorBlock(BlockSetType.COPPER, createTrapdoorProperties(TIN_BLOCK.get())));
+	public static final DeferredBlock<Block> TIN_ORE = registerBlock("tin_ore", () -> new Block(Properties.ORE_STONE));
+	public static final DeferredBlock<Block> DEEPSLATE_TIN_ORE = registerBlock("deepslate_tin_ore", () -> new Block(Properties.ORE_DEEPSLATE));
 
-	public static final DeferredBlock<Block> CHISELED_TIN = registerBlock("chiseled_tin",
-		() -> new Block(TIN));
+	// METHODS
+	private static DeferredBlock<Block> registerTinBlock(String name) {
+		return registerBlock(name, () -> new Block(Properties.GENERIC_TIN));
+	}
 
-	public static final DeferredBlock<Block> TIN_GRATE = registerBlock("tin_grate",
-		() -> new Block(BlockBehaviour.Properties.ofFullCopy(TIN_BLOCK.get())
+	private static Block createGrateBlock(Block baseBlock) {
+		return new Block(BlockBehaviour.Properties.ofFullCopy(baseBlock)
 			.sound(SoundType.COPPER_GRATE)
 			.noOcclusion()
 			.isRedstoneConductor((state, level, pos) -> false)
 			.isSuffocating((state, level, pos) -> false)
 			.isViewBlocking((state, level, pos) -> false)) {
-		@Override
+			@Override
 			public boolean skipRendering(@NotNull BlockState state, @NotNull BlockState adjacentState, @NotNull Direction direction) {
-			return adjacentState.is(this) || super.skipRendering(state, adjacentState, direction);
-		}});
-
-	public static final DeferredBlock<Block> CUT_TIN = registerBlock("cut_tin",
-		() -> new Block(TIN));
-
-	public static final DeferredBlock<StairBlock> CUT_TIN_STAIRS = registerBlock("cut_tin_stairs",
-		() -> new StairBlock(ModBlocks.CUT_TIN.get().defaultBlockState(), (TIN)));
-
-	public static final DeferredBlock<SlabBlock> CUT_TIN_SLAB = registerBlock("cut_tin_slab",
-		() -> new SlabBlock(TIN));
-
-	public static final DeferredBlock<DoorBlock> TIN_DOOR = registerBlock("tin_door",
-		() -> new DoorBlock(BlockSetType.COPPER, BlockBehaviour.Properties.ofFullCopy(TIN_BLOCK.get())
-			.noOcclusion()
-			.pushReaction(PushReaction.DESTROY)));
-
-	public static final DeferredBlock<TrapDoorBlock> TIN_TRAPDOOR = registerBlock("tin_trapdoor",
-		() -> new TrapDoorBlock(BlockSetType.COPPER, BlockBehaviour.Properties.ofFullCopy(TIN_BLOCK.get())
-			.noOcclusion()
-			.isValidSpawn(Blocks::never)));
-
-	public static final DeferredBlock<Block> TIN_ORE = registerBlock("tin_ore",
-		() -> new Block(BlockBehaviour.Properties.of()
-			.mapColor(MapColor.STONE)
-			.instrument(NoteBlockInstrument.BASEDRUM)
-			.requiresCorrectToolForDrops()
-			.strength(3.0f)
-			.explosionResistance(3.0f)
-		));
-
-	public static final DeferredBlock<Block> DEEPSLATE_TIN_ORE = registerBlock("deepslate_tin_ore",
-		() -> new Block(BlockBehaviour.Properties.of()
-			.mapColor(MapColor.DEEPSLATE)
-			.instrument(NoteBlockInstrument.BASEDRUM)
-			.requiresCorrectToolForDrops()
-			.strength(4.5f)
-			.explosionResistance(3.0f)
-			.sound(SoundType.DEEPSLATE)
-		));
-
-	public static final DeferredBlock<Block> RAW_TIN_BLOCK = registerBlock("raw_tin_block",
-		() -> new Block(BlockBehaviour.Properties.of()
-			.mapColor(MapColor.RAW_IRON)
-			.instrument(NoteBlockInstrument.BASEDRUM)
-			.requiresCorrectToolForDrops()
-			.strength(5.5f)
-			.explosionResistance(6.0f)
-		));
-
-	private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
-		DeferredBlock<T> toReturn = BLOCKS.register(name, block);
-		registerBlockItem(name, toReturn);
-		return toReturn;
+				return adjacentState.is(this) || super.skipRendering(state, adjacentState, direction);
+			}
+		};
 	}
 
-	private static  <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block) {
+	private static BlockBehaviour.Properties createDoorProperties(Block baseBlock) {
+		return BlockBehaviour.Properties.ofFullCopy(baseBlock)
+			.noOcclusion()
+			.pushReaction(PushReaction.DESTROY);
+	}
+
+	private static BlockBehaviour.Properties createTrapdoorProperties(Block baseBlock) {
+		return BlockBehaviour.Properties.ofFullCopy(baseBlock)
+			.noOcclusion()
+			.isValidSpawn(Blocks::never);
+	}
+
+	private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> blockSupplier) {
+		DeferredBlock<T> block = BLOCKS.register(name, blockSupplier);
 		ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+		return block;
 	}
 
 	public static void register(IEventBus eventBus) {
