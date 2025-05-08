f0314c0a7de88d431ce13097ac753d393b8b00a1
Disketaa <76956762+Disketaa@users.noreply.github.com>
2025-05-08 19:09:46 +0300

    Localization

--- src/main/java/com/disketaa/harmonium/configuration/ModConfigurationScreens.java
+++ src/main/java/com/disketaa/harmonium/configuration/ModConfigurationScreens.java
@@ -12,9 +12,9 @@ import org.jetbrains.annotations.NotNull;
 
 public class ModConfigurationScreens extends Screen {
 	private final Screen parent;
-	private static final int TEXT_WIDTH = 200;
+	private static final int TEXT_WIDTH = 250;
 	private static final int BUTTON_WIDTH = 100;
-	private static final int ROW_HEIGHT = 20;
+	private static final int BUTTON_HEIGHT = 20;
 	private static final int PADDING = 5;
 
 	public ModConfigurationScreens(Screen parent) {
@@ -33,42 +33,43 @@ public class ModConfigurationScreens extends Screen {
 		addConfigRow(startX, currentY,
 			Component.translatable("config.harmonium.show_harmonium_creative_tab"),
 			Config.SHOW_HARMONIUM_CREATIVE_TAB);
-		currentY += ROW_HEIGHT + PADDING;
+		currentY += BUTTON_HEIGHT + PADDING;
 
 		addConfigRow(startX, currentY,
 			Component.translatable("config.harmonium.add_harmonium_items_to_other_tabs"),
 			Config.ADD_HARMONIUM_ITEMS_TO_OTHER_TABS);
-		currentY += ROW_HEIGHT + PADDING;
+		currentY += BUTTON_HEIGHT + PADDING;
 
 		addConfigRow(startX, currentY,
 			Component.translatable("config.harmonium.tin_generation"),
 			Config.TIN_GENERATION);
-		currentY += ROW_HEIGHT + PADDING;
+		currentY += BUTTON_HEIGHT + PADDING;
 
 		addConfigRow(startX, currentY,
 			Component.translatable("config.harmonium.remove_stone_tools"),
 			Config.REMOVE_STONE_TOOLS);
-		currentY += ROW_HEIGHT + PADDING;
+		currentY += BUTTON_HEIGHT + PADDING;
 
 		addConfigRow(startX, currentY,
 			Component.translatable("config.harmonium.remove_flint_knife"),
 			Config.REMOVE_FLINT_KNIFE);
-		currentY += ROW_HEIGHT + PADDING;
+		currentY += BUTTON_HEIGHT + PADDING;
 
-		addIntConfigRow(startX, currentY,
-			Component.translatable("config.harmonium.tin_button.short_press_duration"),
-			Config.TIN_BUTTON_SHORT_PRESS_DURATION, 20);
-		currentY += ROW_HEIGHT + PADDING;
 
 		addIntConfigRow(startX, currentY,
 			Component.translatable("config.harmonium.tin_button.long_press_duration"),
 			Config.TIN_BUTTON_LONG_PRESS_DURATION, 20);
-		currentY += ROW_HEIGHT + PADDING;
+		currentY += BUTTON_HEIGHT + PADDING;
+
+		addIntConfigRow(startX, currentY,
+			Component.translatable("config.harmonium.tin_button.short_press_duration"),
+			Config.TIN_BUTTON_SHORT_PRESS_DURATION, 20);
+		currentY += BUTTON_HEIGHT + PADDING;
 
 		addIntConfigRow(startX, currentY,
 			Component.translatable("config.harmonium.tin_button.failure_chance"),
 			Config.TIN_BUTTON_FAILURE_CHANCE, 100);
-		currentY += ROW_HEIGHT + PADDING;
+		currentY += BUTTON_HEIGHT + PADDING;
 
 		addRenderableWidget(Button.builder(CommonComponents.GUI_DONE,
 				button -> {
@@ -81,7 +82,7 @@ public class ModConfigurationScreens extends Screen {
 
 	private void addConfigRow(int startX, int y, Component label, ModConfigSpec.BooleanValue configValue) {
 		Component labelWithColon = Component.translatable(label.getString() + ":");
-		addRenderableWidget(new Label(startX, y, TEXT_WIDTH, ROW_HEIGHT, labelWithColon));
+		addRenderableWidget(new Label(startX, y, TEXT_WIDTH, BUTTON_HEIGHT, labelWithColon));
 
 		int buttonX = startX + TEXT_WIDTH + PADDING;
 
@@ -89,17 +90,17 @@ public class ModConfigurationScreens extends Screen {
 				Component.translatable("options.on"),
 				Component.translatable("options.off"))
 			.withInitialValue(configValue.get())
-			.create(buttonX, y, BUTTON_WIDTH, ROW_HEIGHT,
+			.create(buttonX, y, BUTTON_WIDTH, BUTTON_HEIGHT,
 				Component.empty(),
 				(btn, value) -> configValue.set(value)));
 	}
 
 	private void addIntConfigRow(int startX, int y, Component label, ModConfigSpec.IntValue configValue, int max) {
-		addRenderableWidget(new Label(startX, y, TEXT_WIDTH, ROW_HEIGHT, label));
+		addRenderableWidget(new Label(startX, y, TEXT_WIDTH, BUTTON_HEIGHT, label));
 
 		int fieldX = startX + TEXT_WIDTH + PADDING;
 
-		EditBox field = new EditBox(this.font, fieldX, y, BUTTON_WIDTH, ROW_HEIGHT, Component.empty());
+		EditBox field = new EditBox(this.font, fieldX, y, BUTTON_WIDTH, BUTTON_HEIGHT, Component.empty());
 		field.setValue(String.valueOf(configValue.get()));
 		field.setResponder(text -> {
 			try {
@@ -134,11 +135,13 @@ public class ModConfigurationScreens extends Screen {
 
 		@Override
 		public void renderWidget(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
+			int textY = this.getY() + (this.height - ModConfigurationScreens.this.font.lineHeight) / 2 + 1;
+
 			guiGraphics.drawString(
 				ModConfigurationScreens.this.font,
 				this.getMessage(),
 				this.getX(),
-				this.getY(),
+				textY,
 				0xFFFFFF,
 				true
 			);
